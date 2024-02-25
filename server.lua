0bf31181fe227b26cb325d4bdb6c34e4593ae1a7
borsuczyna <libxxerty@gmail.com>
2024-02-25 13:44:52 +0100

    export functions in meta

--- server.lua
+++ server.lua
@@ -95,11 +95,3 @@ addEventHandler('onPlayerQuit', root, function()
         deleteCustomTrain(train)
     end
 end)
-
--- debug
--- addEventHandler('onPlayerResourceStart', root, function(resource)
---     if getResourceName(resource) ~= 'trains' then return end
---     local train = createCustomTrain(537, getRandomPlayer(), 2048.52466, 1530.22681, 11.67188)
---     setTrainTrack(train, 2)
---     setTrainSpeed(train, 0)
--- end)
